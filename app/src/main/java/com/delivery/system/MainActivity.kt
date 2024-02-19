6ab0f5971aa84aeaa6fd02adfa1c4d4ae43247f7
znematov <zubaidullahnematov@gmail.com>
2024-02-19 21:59:10 +0500

    Placing elements on Login page

--- app/src/main/java/com/delivery/system/MainActivity.kt
+++ app/src/main/java/com/delivery/system/MainActivity.kt
@@ -2,10 +2,19 @@ package com.delivery.system
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.login)
+
+        val loginButton = findViewById<Button>(R.id.loginButton)
+        loginButton.setOnClickListener{
+            val toast = Toast(this)
+            toast.setText("Logged in")
+            toast.show()
+        }
     }
 }
