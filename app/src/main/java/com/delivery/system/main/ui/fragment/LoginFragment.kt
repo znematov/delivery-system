0200be9545cf8cf3d1b4eec35789ae983d267f35
znematov <zubaidullahnematov@gmail.com>
2024-03-26 10:57:05 +0500

    setting up layouts

---
+++ app/src/main/java/com/delivery/system/main/ui/fragment/LoginFragment.kt
@@ -0,0 +1,7 @@
+package com.delivery.system.main.ui.fragment
+
+import com.delivery.system.R
+import com.delivery.system.core.ui.BaseFragment
+
+class LoginFragment : BaseFragment(R.layout.login_fragment) {
+}
